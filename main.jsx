826619617750f7e202347dab0286cf063557bff2
Toqa Tamer <toqatamerg@gmail.com>
2026-09-15 00:30:01 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import OlistDashboard from "./OlistDashboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <OlistDashboard />
+  </React.StrictMode>
+);
