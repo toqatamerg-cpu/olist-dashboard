b567260eb1b50788573d033fbd3bb688fd6b26f5
Toqa Tamer <toqatamerg@gmail.com>
2026-09-15 00:29:45 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import OlistDashboard from "./OlistDashboard.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <OlistDashboard />
-  </React.StrictMode>
-);
